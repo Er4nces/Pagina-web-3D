fe46266aade67b09c82ae460584af832c6fb14fc
Jorge Gil <159541567+Er4nces@users.noreply.github.com>
2024-12-05 08:00:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script for navigation bar
+const bar = document.getElementById(ˈbarˈ);
+const nav = document.getElementById(ˈnavbarˈ)
+
+if (bar){
+    bar.addEventListener(ˈclickˈ, ()=>{
+        nav.classList.add(ˈactiveˈ);
+    })
+}
